ee6ca55f88b308504a89078e9899a9a6e17fdb5c
vkuzbida <vkuzbida@n-ix.com>
2016-10-25 15:17:30 +0300

    establish connection to DB

--- node_sequelize/app.js
+++ node_sequelize/app.js
@@ -1,3 +1,45 @@
-/**
- * Created by cfuiteam on 24.10.16.
- */
+var Sequelize = require('sequelize'),
+    mysql = require('mysql');
+
+var sequelize = new Sequelize('TrainingDay', 'root', 'password', {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
+});
+
+sequelize
+    .authenticate()
+    .then(function(err) {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(function (err) {
+        console.log('Unable to connect to the database:', err);
+    });
+
+
+var User = sequelize.define('user', {
+    firstName: {
+        type: Sequelize.STRING
+    },
+    lastName: {
+        type: Sequelize.STRING
+    }
+});
+
+// force: true will drop the table if it already exists
+User.sync().then(function () {
+    // Table created
+    return User.create({
+        firstName: 'John',
+        lastName: 'Hancock'
+    });
+});
+
+User.findAll().then(function(users) {
+    console.log(users)
+})
